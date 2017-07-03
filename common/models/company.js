24122a5b06155435034048421b8f5fdcddfac197
HaiderMalik <haidermalik504@gmail.com>
2017-07-03 10:35:15 +0500

    Add Custom Remote Greet Method

--- common/models/company.js
+++ common/models/company.js
@@ -1,5 +1,23 @@
 'use strict';
 
 module.exports = function(Company) {
+ 
+ Company.greet = function(msg, cb) {
+     cb(null,msg);
+ }
 
+ Company.remoteMethod('greet', {
+     accepts : {
+         arg: 'msg',
+         type: 'string'
+     },
+     returns : { 
+         arg : 'greeting',
+         type: 'string'
+     },
+     http :{
+         verb : 'get'
+     }
+    
+ });
 };
