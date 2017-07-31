3826f4fae22c1d3134730e1aa027be8f59a85bae
HaiderMalik <haidermalik504@gmail.com>
2017-07-31 10:16:04 +0500

    add two new properties in product-model

--- server/boot/automigrate.js
+++ server/boot/automigrate.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function(app) {
- app.dataSources.mongodb.automigrate(['Product','Company','Category','Account'],err => {
+ app.dataSources.mongodb.autoupdate(['Product','Company','Category','Account'],err => {
      if(err) throw err;
      console.log('Models Synced!');
  })
